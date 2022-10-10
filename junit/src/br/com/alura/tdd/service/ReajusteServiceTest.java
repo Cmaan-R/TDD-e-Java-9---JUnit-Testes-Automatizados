a185c0a1b6516b6174353d1d3e2b2327b70f6a28
Windows 10 <Windows 10@192.168.200.171>
2022-10-10 15:50:42 -0300

    Update

--- junit/src/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ junit/src/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -12,11 +13,18 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
 
+	private ReajusteService service;
+	private Funcionario funcionario;
+
+	@BeforeEach
+	public void inicializar() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+
 	@Test
 	public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
-
+		
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
 
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
@@ -24,8 +32,6 @@ public class ReajusteServiceTest {
 
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorcentoQuandoDesempenhoForBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
 
 		service.concederReajuste(funcionario, Desempenho.BOM);
 
@@ -34,9 +40,6 @@ public class ReajusteServiceTest {
 
 	@Test
 	public void reajusteDeveriaSerDeVintePorcentoQuandoDesempenhoForOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
-
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
 
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
